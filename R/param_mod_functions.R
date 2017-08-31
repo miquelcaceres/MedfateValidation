50fee202bf0ea28eca28ceaa1a3af2c8b6f2a6f5
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-08-31 13:04:00 +0200

    input custom modification function added

--- R/param_mod_functions.R
+++ R/param_mod_functions.R
@@ -31,3 +31,46 @@ SpParams_mod <- function(SpParams, customParams) {
   # return the new SpParams
   return(SpParams)
 }
+
+#' Modification of swb input object
+#'
+#' This function looks for and modifies input parameters with the values
+#' supplied in the customParams.txt file
+#'
+#' @family CustomMods
+#'
+#' @param swbInput An object of class \code{swbInput} as obtained from
+#'   \code{\link{forest2swbInput}} or \code{\link{swbInput}} functions from
+#'   \code{medfate} package
+#'
+#' @param customParams Data frame with the custom parameters info, usually
+#'   the result of \code{\link{load_customParams}}
+#'
+#' @export
+
+inputMod <- function(swbInput, customParams) {
+
+  # get the names of the custom params and the input tables
+  custom <- names(customParams)
+  above_par <- names(swbInput$above)
+  # below_par <- names(swbInput$below)
+
+
+  # iterate between the custom params
+  for (param in custom) {
+    # check if the param exists in above
+    if (param %in% above_par) {
+      # subsitute it!
+      swbInput$above[[param]] <- customParams[[param]]
+    }
+
+    # check if the param exists in below
+    # if (param %in% below_par) {
+    #   # subsitute it!
+    #   swbInput$below[[param]] <- customParams[[param]]
+    # }
+  }
+
+  # return the new input
+  return(swbInput)
+}
