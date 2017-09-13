d1237b0185541feaf516e6747806c84b4f43a08e
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-09-13 16:36:40 +0200

    added statistics_summary function and improved bias_calculator

--- R/statistics_functions.R
+++ R/statistics_functions.R
@@ -57,7 +57,149 @@ r_squared_calculator <- function(real, predicted) {
 
 bias_calculator <- function(real, predicted) {
   # calculate bias
-  res <- mean(real - predicted)
+  res <- mean(predicted - real)
   # return it
   return(res)
+}
+
+#' Statistics summary for a given variable
+#'
+#' This function calculates the different statistics for a given variable
+#' accounting for the models performed (simple, complex, both)
+#'
+#' @param var Character with the variable name
+#'
+#' @param models list with the dataframes of models results, typically the
+#'   result of \code{\link{saveRes}}
+#'
+#' @param measured_data Data frame with the "real" data
+#'
+#' @param soil Soil object needed to calculate SWC from W
+#'
+#' @export
+
+statistics_summary <- function(var, models, measured_data, soil) {
+
+  # if SWC
+  if (var == 'SWC') {
+
+    # check which models have been performed and get the statistics
+    if (!is.null(models[['simple']])) {
+      SWC_MAE_simple <- MAE_calculator(measured_data[['SWC']],
+                                       models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_r_sq_simple <- r_squared_calculator(measured_data[['SWC']],
+                                              models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_bias_simple <- bias_calculator(measured_data[['SWC']],
+                                         models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+    } else {
+      # if the model is not performed, the statistics are NAs
+      SWC_MAE_simple <- NA
+      SWC_r_sq_simple <- NA
+      SWC_bias_simple <- NA
+    }
+
+    if (!is.null(models[['complex']])) {
+      SWC_MAE_complex <- MAE_calculator(measured_data[['SWC']],
+                                        models[['complex']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_r_sq_complex <- r_squared_calculator(measured_data[['SWC']],
+                                               models[['complex']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_bias_complex <- bias_calculator(measured_data[['SWC']],
+                                          models[['complex']][['W.1']] * soil$Theta_FC[[1]])
+    } else {
+      SWC_MAE_complex <- NA
+      SWC_r_sq_complex <- NA
+      SWC_bias_complex <- NA
+    }
+
+    if (!is.null(models[['simple']]) & !is.null(models[['complex']])) {
+      SWC_MAE_both <- MAE_calculator(models[['complex']][['W.1']] * soil$Theta_FC[[1]],
+                                     models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_r_sq_both <- r_squared_calculator(models[['complex']][['W.1']] * soil$Theta_FC[[1]],
+                                            models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+      SWC_bias_both <- bias_calculator(models[['complex']][['W.1']] * soil$Theta_FC[[1]],
+                                       models[['simple']][['W.1']] * soil$Theta_FC[[1]])
+    } else {
+      SWC_MAE_both <- NA
+      SWC_r_sq_both <- NA
+      SWC_bias_both <- NA
+    }
+
+    # build the res object and return it!
+    res <- list(
+      SWC_MAE_simple = SWC_MAE_simple,
+      SWC_r_sq_simple = SWC_r_sq_simple,
+      SWC_bias_simple = SWC_bias_simple,
+      SWC_MAE_complex = SWC_MAE_complex,
+      SWC_r_sq_complex = SWC_r_sq_complex,
+      SWC_bias_complex = SWC_bias_complex,
+      SWC_MAE_both = SWC_MAE_both,
+      SWC_r_sq_both = SWC_r_sq_both,
+      SWC_bias_both = SWC_bias_both
+    )
+
+    return(res)
+  }
+
+  # if Eplanttot
+  if (var == 'Eplanttot') {
+
+    # check which models have been performed and get the statistics
+    if (!is.null(models[['simple']])) {
+      Eplanttot_MAE_simple <- MAE_calculator(measured_data[['Eplanttot']],
+                                             models[['simple']][['Eplanttot']])
+      Eplanttot_r_sq_simple <- r_squared_calculator(measured_data[['Eplanttot']],
+                                                    models[['simple']][['Eplanttot']])
+      Eplanttot_bias_simple <- bias_calculator(measured_data[['Eplanttot']],
+                                               models[['simple']][['Eplanttot']])
+    } else {
+      # if the model is not performed, the statistics are NAs
+      Eplanttot_MAE_simple <- NA
+      Eplanttot_r_sq_simple <- NA
+      Eplanttot_bias_simple <- NA
+    }
+
+    if (!is.null(models[['complex']])) {
+      Eplanttot_MAE_complex <- MAE_calculator(measured_data[['Eplanttot']],
+                                              models[['complex']][['Eplanttot']])
+      Eplanttot_r_sq_complex <- r_squared_calculator(measured_data[['Eplanttot']],
+                                                     models[['complex']][['Eplanttot']])
+      Eplanttot_bias_complex <- bias_calculator(measured_data[['Eplanttot']],
+                                                models[['complex']][['Eplanttot']])
+    } else {
+      Eplanttot_MAE_complex <- NA
+      Eplanttot_r_sq_complex <- NA
+      Eplanttot_bias_complex <- NA
+    }
+
+    if (!is.null(models[['simple']]) & !is.null(models[['complex']])) {
+      Eplanttot_MAE_both <- MAE_calculator(models[['complex']][['Eplanttot']],
+                                           models[['simple']][['Eplanttot']])
+      Eplanttot_r_sq_both <- r_squared_calculator(models[['complex']][['Eplanttot']],
+                                                  models[['simple']][['Eplanttot']])
+      Eplanttot_bias_both <- bias_calculator(models[['complex']][['Eplanttot']],
+                                             models[['simple']][['Eplanttot']])
+    } else {
+      Eplanttot_MAE_both <- NA
+      Eplanttot_r_sq_both <- NA
+      Eplanttot_bias_both <- NA
+    }
+
+    # build the res object and return it!
+    res <- list(
+      Eplanttot_MAE_simple = Eplanttot_MAE_simple,
+      Eplanttot_r_sq_simple = Eplanttot_r_sq_simple,
+      Eplanttot_bias_simple = Eplanttot_bias_simple,
+      Eplanttot_MAE_complex = Eplanttot_MAE_complex,
+      Eplanttot_r_sq_complex = Eplanttot_r_sq_complex,
+      Eplanttot_bias_complex = Eplanttot_bias_complex,
+      Eplanttot_MAE_both = Eplanttot_MAE_both,
+      Eplanttot_r_sq_both = Eplanttot_r_sq_both,
+      Eplanttot_bias_both = Eplanttot_bias_both
+    )
+
+    return(res)
+  }
+
+  # TODO transpiration by cohort
+
 }
