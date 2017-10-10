18fdeeb7261818a38e44e4dd36aac06712a6e364
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-10-10 17:35:02 +0200

    added main functions for the two processes

--- R/day_by_day_functions.R
+++ R/day_by_day_functions.R
@@ -30,34 +30,42 @@ transp_day_by_day <- function(input, meteoData, soilData,
 
   for (i in 1:length(dates)) {
 
-    # copy of input to avoid modifications
-    loop_input <- input
+    # check if we have SWC measured
+    if (is.na(measuredData[i, 'SWC'])) {
+      # fill the res objects by cohort
+      for (j in 1:n_coh) {
+        e_coh[i,j] <- NA
+      }
+    } else {
+      # copy of input to avoid modifications
+      loop_input <- input
 
-    # generate the soil object replacing W with the measured value
-    soilData[['W']] <- measuredData[i, 'SWC']/soilData[['FC']]
-    soil_object <- buildSoil(soilData)
+      # generate the soil object replacing W with the measured value
+      soilData[['W']] <- measuredData[i, 'SWC']/soilData[['FC']]
+      soil_object <- buildSoil(soilData)
 
-    # swb.day
-    temp_res <- medfate::swb.day(
-      loop_input, soil_object, dates[[i]], meteoData[i, 'DOY'],
-      meteoData[i, 'MinTemperature'], meteoData[i, 'MaxTemperature'],
-      meteoData[i, 'MinRelativeHumidity'], meteoData[i, 'MaxRelativeHumidity'],
-      meteoData[i, 'Radiation'], meteoData[i, 'WindSpeed'],
-      terrainData[['latitude']], terrainData[['elevation']],
-      terrainData[['slope']], terrainData[['aspect']],
-      meteoData[i, 'Precipitation']
-    )
+      # swb.day
+      temp_res <- medfate::swb.day(
+        loop_input, soil_object, dates[[i]], meteoData[i, 'DOY'],
+        meteoData[i, 'MinTemperature'], meteoData[i, 'MaxTemperature'],
+        meteoData[i, 'MinRelativeHumidity'], meteoData[i, 'MaxRelativeHumidity'],
+        meteoData[i, 'Radiation'], meteoData[i, 'WindSpeed'],
+        terrainData[['latitude']], terrainData[['elevation']],
+        terrainData[['slope']], terrainData[['aspect']],
+        meteoData[i, 'Precipitation']
+      )
 
-    # fill the res objects by cohort
-    for (j in 1:n_coh) {
-      e_coh[i,j] <- temp_res[['EplantCoh']][j]
+      # fill the res objects by cohort
+      for (j in 1:n_coh) {
+        e_coh[i,j] <- temp_res[['EplantCoh']][j]
+      }
     }
 
   }
 
   # build the final res
   res <- as.data.frame(e_coh)
-  etot <- rowsum(res)
+  etot <- rowSums(res)
   res <- res %>%
     dplyr::mutate(Dates = dates, Eplanttot = etot) %>%
     dplyr::select(Dates, Eplanttot, dplyr::everything())
