f6d1ae6fae90a1d1002ccbfb6c67090f71e8bf3f
miquelcaceres <miquelcaceres@gmail.com>
2020-03-17 08:56:43 +0100

    get/set path

--- R/site_data_path_functions.R
+++ R/site_data_path_functions.R
@@ -1,3 +1,9 @@
 pkg.globals <- new.env()
 
 pkg.globals$site_data_path <- "D:/Rpackages/medfate/validation/"
+
+set_data_path<-function(path) {
+  pkg.globals$site_data_path <- path
+}
+
+get_data_path<-function(){return(pkg.globals$site_data_path)}
